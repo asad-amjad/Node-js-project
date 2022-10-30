8c2a8b2baee03433754cf69eb608e890f30fd004
asad-amjad <asad.amjad.pk@gmail.com>
2022-10-30 08:37:59 -0700

    write file

--- routes.js
+++ routes.js
@@ -21,16 +21,15 @@ const requestHandler = (req, res) => {
       body.push(chunk);
     });
 
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      //   console.log(parsedBody);
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.text", message);
+      fs.writeFile("message.text", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
