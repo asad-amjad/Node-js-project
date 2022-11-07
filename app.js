0ea13ee0ccc38a60233638fa93b03f364baa7a69
asad-amjad <asad.amjad.pk@gmail.com>
2022-11-07 10:05:50 -0800

    install & middleware

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const http = require("http");
-const route = require("./routes");
+// const route = require("./routes");
 
-const server = http.createServer(route);
+const express = require('express');
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('in the middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('in another middleware')
+    res.send('<h1>Hello world</h1>')
+    // next()
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
