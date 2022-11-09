da57bea049a821131d8868c789777c9c2d1e5d35
asad-amjad <asad.amjad.pk@gmail.com>
2022-11-09 22:18:14 +0500

    send form data and redirect

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const http = require("http");
-// const route = require("./routes");
 
 const express = require('express');
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('in the middleware')
-    next()
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('in another middleware')
-    res.send('<h1>Hello world</h1>')
-    // next()
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>')
 })
 
 const server = http.createServer(app);
