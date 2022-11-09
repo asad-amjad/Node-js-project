1499474deb4e82bfa0b97ec4c44dd431696e2886
asad-amjad <asad.amjad.pk@gmail.com>
2022-11-09 22:29:50 +0500

    config body parser middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
-
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
