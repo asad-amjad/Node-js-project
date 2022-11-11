b46a94aa5791948e1a01f49cecc352d3d90687f7
asad-amjad <asad.amjad.pk@gmail.com>
2022-11-11 10:54:25 +0500

    sharing data across request

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log("shopPage products",adminData.products)
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
